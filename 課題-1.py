d52e36b3f36ef3bb74b84078c9d944d098d67276
Yamayuu2021 <97350743+Yamayuu2021@users.noreply.github.com>
2022-01-13 15:30:01 +0900

    Delete 課題-1.py

--- 課題-1.py
+++
@@ -1,9 +0,0 @@
-for i in range(1,101):
-    if i % 3 == 0:
-        print("勉強したい")
-    elif i % 5 == 0:
-        print("就活したい")
-    elif i % 3 and i % 5 == 0:
-        print("だいがく爆破したい")
-    else:
-        print(i)
