78eab66c261e6db5e31fb289550d2caeac94ec92
Yamayuu2021 <97350743+Yamayuu2021@users.noreply.github.com>
2022-01-14 00:50:07 +0900

    Update kadai-1.py

--- kadai-1.py
+++ kadai-1.py
@@ -1,4 +1,6 @@
 for i in range(1,101):
+for i in range(1,201):
+    
     if i % 3 == 0:
         print("勉強したい")
     elif i % 5 == 0:
@@ -6,4 +8,5 @@ for i in range(1,101):
     elif i % 3 and i % 5 == 0:
         print("だいがく爆破したい")
     else:
+        print(i)
         print(i)
